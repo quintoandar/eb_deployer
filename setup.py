a5061543dba6a24afe3ae2c73476e54d88b64acf
Diego Queiroz <dqueiroz@quintoandar.com.br>
2015-07-08 21:30:46 -0300

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='quintoandar_eb_deployer',
     description='Make your project publishes itself to AWS',
-    version='1.16',
+    version='1.17',
     author='Diego Queiroz',
     author_email='diego.queiroz@gmail.com',
     license='MIT',
